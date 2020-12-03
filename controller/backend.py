8f761d8bccca7534a3e62c5a5457ed0356a471b8
Jaroslav <>
2020-12-03 19:21:13 +0200

    added rb for time period / taking columns for day period

--- controller/backend.py
+++ controller/backend.py
@@ -3,7 +3,7 @@ import cx_Oracle
 from config.config import config_dsn, config_connect, query
 
 
-def get_columns():
+def get_columns_2g_h():
         config_data = config_dsn()
         dsn = cx_Oracle.makedsn(config_data[1], config_data[2], service_name=config_data[3])
         config_connection = config_connect()
@@ -20,3 +20,20 @@ def get_columns():
         return result_list
         cursor.close()
 
+
+def get_columns_2g_d():
+    config_data = config_dsn()
+    dsn = cx_Oracle.makedsn(config_data[1], config_data[2], service_name=config_data[3])
+    config_connection = config_connect()
+    for i in range(len(config_connection)):
+        connection = cx_Oracle.connect(config_connection[0], config_connection[1], dsn, encoding="UTF-8")
+    cursor = connection.cursor()
+    config_query = query()
+    for q in range(len(config_query)):
+        cursor.execute(config_query[1])
+    result_set = cursor.fetchall()
+    result_list = []
+    for row in result_set:
+        result_list.append(row)
+    return result_list
+    cursor.close()
--- gui/graphic_view.py
+++ gui/graphic_view.py
@@ -7,7 +7,7 @@ from matplotlib.backends.backend_tkagg import (
 from matplotlib.figure import Figure
 from tkcalendar import DateEntry, Calendar
 from config.config import query
-from controller.backend import get_columns
+from controller.backend import get_columns_2g_h
 
 class GraphicView:
 
@@ -19,52 +19,76 @@ class GraphicView:
         self.main_window.wm_title("Statistics")
 
         self.input_data_fr = tkinter.Frame(self.main_window,relief='raised', borderwidth=1, background="misty rose")
-        self.input_cell_name_lb = tkinter.Label(self.input_data_fr,text='CellName:')
-        self.input_cell_name_ent = tkinter.Entry(self.input_data_fr, width='22')
-        self.input_cell_ci_lb = tkinter.Label(self.input_data_fr, text='CellId:')
-        self.input_cell_ci_ent = tkinter.Entry(self.input_data_fr, width='22')
+        self.input_data_fr.pack(side='left', fill='y')
 
-        self.cal_start_lab = tkinter.Label(self.input_data_fr, text='Choose date').pack(side = 'top',padx=10, pady=10)
+        self.cal_start_lab = tkinter.Label(self.input_data_fr, text='Choose date').pack(side='top', padx=10, pady=10)
         self.cal_start_ent = DateEntry(self.input_data_fr, width=12, background='red',
-                        foreground='white', borderwidth=2, year=2020,calendar_cursor="hand1",selectmode='day')
-        self.cal_start_ent.pack(padx=10, pady=10)
-        self.cal_end_ent = DateEntry(self.input_data_fr, width=12, background='red',
                                        foreground='white', borderwidth=2, year=2020, calendar_cursor="hand1",
                                        selectmode='day')
+        self.cal_start_ent.pack(padx=10, pady=10)
+
+        self.cal_end_ent = DateEntry(self.input_data_fr, width=12, background='red',
+                                     foreground='white', borderwidth=2, year=2020, calendar_cursor="hand1",
+                                     selectmode='day')
         self.cal_end_ent.pack(padx=10, pady=10)
 
+        self.var_tech = tkinter.IntVar()
+        self.choose_tech_gsm_rb = tkinter.Radiobutton(self.input_data_fr, text='GSM', variable=self.var_tech, value=1, command='')
+        self.choose_tech_gsm_rb.pack(side='top')
+        self.choose_tech_wcdma_rb = tkinter.Radiobutton(self.input_data_fr, text='WCDMA', variable=self.var_tech, value=2, command='')
+        self.choose_tech_wcdma_rb.pack(side='top')
+        self.choose_tech_lte_rb = tkinter.Radiobutton(self.input_data_fr, text='LTE', variable=self.var_tech, value=3, command='')
+        self.choose_tech_lte_rb.pack(side='top')
 
-        self.columns = get_columns()
+        self.var_period = tkinter.IntVar()
+        self.choose_hourly_rb = tkinter.Radiobutton(self.input_data_fr, text='Hourly', variable=self.var_period, value=1, command='')
+        self.choose_hourly_rb.pack(side='top')
+        self.choose_daily_rb = tkinter.Radiobutton(self.input_data_fr, text='Daily', variable=self.var_period, value=2, command='')
+        self.choose_daily_rb.pack(side='top')
+
+        self.input_cell_name_lb = tkinter.Label(self.input_data_fr,text='CellName:')
+        self.input_cell_name_lb.pack(side='top')
+
+        self.input_cell_name_ent = tkinter.Entry(self.input_data_fr, width='22')
+        self.input_cell_name_ent.pack(side='top', padx=5, pady=5)
+
+        self.input_cell_ci_lb = tkinter.Label(self.input_data_fr, text='CellId:')
+        self.input_cell_ci_lb.pack(side='top')
+
+        self.input_cell_ci_ent = tkinter.Entry(self.input_data_fr, width='22')
+        self.input_cell_ci_ent.pack(side='top', padx=5, pady=5)
 
 
+
+
+        self.columns = get_columns_2g_h()
         self.om_chosen1 = tkinter.StringVar()
 
         self.cb1 = ttk.Combobox(self.input_data_fr, values=self.columns)
-        self.cb2 = ttk.Combobox(self.input_data_fr, values=self.columns)
-        self.cb3 = ttk.Combobox(self.input_data_fr, values=self.columns)
-        self.cb4 = ttk.Combobox(self.input_data_fr, values=self.columns)
-        self.cb5 = ttk.Combobox(self.input_data_fr, values=self.columns)
-        self.cb6 = ttk.Combobox(self.input_data_fr, values=self.columns)
-        self.cb7 = ttk.Combobox(self.input_data_fr, values=self.columns)
-        self.cb8 = ttk.Combobox(self.input_data_fr, values=self.columns)
-
-
-
-
-
-        self.input_cell_ci_lb.pack(side='top')
-        self.input_cell_ci_ent.pack(side='top', padx=5, pady=5)
-        self.input_cell_name_lb.pack(side='top')
-        self.input_cell_name_ent.pack(side='top',padx=5, pady=5)
-        self.input_data_fr.pack(side='left',fill='y')
         self.cb1.pack()
+        self.cb2 = ttk.Combobox(self.input_data_fr, values=self.columns)
         self.cb2.pack()
+        self.cb3 = ttk.Combobox(self.input_data_fr, values=self.columns)
         self.cb3.pack()
+        self.cb4 = ttk.Combobox(self.input_data_fr, values=self.columns)
         self.cb4.pack()
+        self.cb5 = ttk.Combobox(self.input_data_fr, values=self.columns)
         self.cb5.pack()
+        self.cb6 = ttk.Combobox(self.input_data_fr, values=self.columns)
         self.cb6.pack()
+        self.cb7 = ttk.Combobox(self.input_data_fr, values=self.columns)
         self.cb7.pack()
+        self.cb8 = ttk.Combobox(self.input_data_fr, values=self.columns)
         self.cb8.pack()
+
+
+
+
+
+
+
+
+
 
         f = Figure(figsize=(8, 2), dpi=100)
         a = f.add_subplot(111)
