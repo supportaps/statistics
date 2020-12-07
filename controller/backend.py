f0792d7d3956d97fb1a4f6128472858e46152289
Jaroslav <>
2020-12-07 02:06:49 +0200

    added method to choose heads in depence of tech

--- controller/backend.py
+++ controller/backend.py
@@ -22,54 +22,66 @@ class GetKpi:
     def get_columns_2g_h(self):
         cursor, result_list = self.db_conn()
         config_query = self.config.query_head_2gh()
+        print("2ghquery", config_query)
         cursor.execute(config_query)
         result_set = cursor.fetchall()
         result_list.append(result_set)
-        print("rs",result_list)
+        print("rs2gh",result_list)
         return result_set
         cursor.close()
 
     def get_columns_2g_d(self):
         cursor, result_list = self.db_conn()
         config_query = self.config.query_head_2gd()
+        print("2gdquery", config_query)
         cursor.execute(config_query)
         result_set = cursor.fetchall()
         result_list.append(result_set)
-        return result_list
+        print("rs2gd", result_list)
+        return result_set
         cursor.close()
 
     def get_columns_3g_h(self):
         cursor, result_list = self.db_conn()
         config_query = self.config.query_head_3gh()
+        print("3ghquery",config_query)
         cursor.execute(config_query)
         result_set = cursor.fetchall()
         result_list.append(result_set)
+        print("rs3gh", result_list)
         return result_set
         cursor.close()
 
     def get_columns_3g_d(self):
         cursor, result_list = self.db_conn()
         config_query = self.config.query_head_3gd()
+        print("3gdquery", config_query)
         cursor.execute(config_query)
         result_set = cursor.fetchall()
         result_list.append(result_set)
+        print("rs3gd", result_list)
         return result_set
         cursor.close()
 
     def get_columns_4g_h(self):
         cursor, result_list = self.db_conn()
         config_query = self.config.query_head_4gh()
+        print("4ghquery", config_query)
         cursor.execute(config_query)
         result_set = cursor.fetchall()
         result_list.append(result_set)
+        print("rs4gh", result_list)
         return result_set
         cursor.close()
 
     def get_columns_4g_d(self):
         cursor, result_list = self.db_conn()
         config_query = self.config.query_head_4gd()
+        print("4gdquery", config_query)
         cursor.execute(config_query)
         result_set = cursor.fetchall()
         result_list.append(result_set)
+        print("rs4gd", result_list)
         return result_set
         cursor.close()
+
--- gui/graphic_view.py
+++ gui/graphic_view.py
@@ -16,6 +16,13 @@ class GraphicView:
         self.columns = GetKpi()
         self.x = x
         self.y = y
+        self.column_2gh = self.columns.get_columns_2g_h()
+        self.column_2gd = self.columns.get_columns_2g_d()
+        self.column_3gh = self.columns.get_columns_3g_h()
+        self.column_3gd = self.columns.get_columns_3g_d()
+        self.column_4gh = self.columns.get_columns_4g_h()
+        self.column_4gd = self.columns.get_columns_4g_d()
+
 
         self.main_window = tkinter.Tk()
         self.main_window.wm_title("Statistics")
@@ -34,20 +41,22 @@ class GraphicView:
                                      selectmode='day')
         self.cal_end_ent.pack(padx=10, pady=10)
 
+
+
         self.var_tech = tkinter.IntVar()
         self.var_tech.set(0)
-        self.choose_tech_gsm_rb = tkinter.Radiobutton(self.input_data_fr, text='GSM', variable=self.var_tech, value=1, command='')
+        self.choose_tech_gsm_rb = tkinter.Radiobutton(self.input_data_fr, text='GSM', variable=self.var_tech, value=1, command=self.rb_tech_call)
         self.choose_tech_gsm_rb.pack(side='top')
-        self.choose_tech_wcdma_rb = tkinter.Radiobutton(self.input_data_fr, text='WCDMA', variable=self.var_tech, value=2, command='')
+        self.choose_tech_wcdma_rb = tkinter.Radiobutton(self.input_data_fr, text='WCDMA', variable=self.var_tech, value=2, command=self.rb_tech_call)
         self.choose_tech_wcdma_rb.pack(side='top')
-        self.choose_tech_lte_rb = tkinter.Radiobutton(self.input_data_fr, text='LTE', variable=self.var_tech, value=3, command='')
+        self.choose_tech_lte_rb = tkinter.Radiobutton(self.input_data_fr, text='LTE', variable=self.var_tech, value=3, command=self.rb_tech_call)
         self.choose_tech_lte_rb.pack(side='top')
 
         self.var_period = tkinter.IntVar()
-        self.var_tech.set(0)
-        self.choose_hourly_rb = tkinter.Radiobutton(self.input_data_fr, text='Hourly', variable=self.var_period, value=1, command='')
+        self.var_period.set(0)
+        self.choose_hourly_rb = tkinter.Radiobutton(self.input_data_fr, text='Hourly', variable=self.var_period, value=1, command=self.rb_tech_call)
         self.choose_hourly_rb.pack(side='top')
-        self.choose_daily_rb = tkinter.Radiobutton(self.input_data_fr, text='Daily', variable=self.var_period, value=2, command='')
+        self.choose_daily_rb = tkinter.Radiobutton(self.input_data_fr, text='Daily', variable=self.var_period, value=2, command=self.rb_tech_call)
         self.choose_daily_rb.pack(side='top')
 
         self.input_cell_name_lb = tkinter.Label(self.input_data_fr,text='CellName:')
@@ -62,29 +71,36 @@ class GraphicView:
         self.input_cell_ci_ent = tkinter.Entry(self.input_data_fr, width='22')
         self.input_cell_ci_ent.pack(side='top', padx=5, pady=5)
 
-
-
-
-        self.column = self.columns.get_columns_2g_h()
         self.om_chosen1 = tkinter.StringVar()
 
-        self.cb1 = ttk.Combobox(self.input_data_fr, values=self.columns)
+        self.result_column_cbx = self.rb_tech_call()
+        print(self.result_column_cbx)
+        self.cb_items = []
+        self.cb1 = ttk.Combobox(self.input_data_fr)
         self.cb1.pack()
-        self.cb2 = ttk.Combobox(self.input_data_fr, values=self.columns)
+        self.cb2 = ttk.Combobox(self.input_data_fr)
         self.cb2.pack()
-        self.cb3 = ttk.Combobox(self.input_data_fr, values=self.columns)
+        self.cb3 = ttk.Combobox(self.input_data_fr)
         self.cb3.pack()
-        self.cb4 = ttk.Combobox(self.input_data_fr, values=self.columns)
+        self.cb4 = ttk.Combobox(self.input_data_fr)
         self.cb4.pack()
-        self.cb5 = ttk.Combobox(self.input_data_fr, values=self.columns)
+        self.cb5 = ttk.Combobox(self.input_data_fr)
         self.cb5.pack()
-        self.cb6 = ttk.Combobox(self.input_data_fr, values=self.columns)
+        self.cb6 = ttk.Combobox(self.input_data_fr)
         self.cb6.pack()
-        self.cb7 = ttk.Combobox(self.input_data_fr, values=self.columns)
+        self.cb7 = ttk.Combobox(self.input_data_fr)
         self.cb7.pack()
-        self.cb8 = ttk.Combobox(self.input_data_fr, values=self.columns)
+        self.cb8 = ttk.Combobox(self.input_data_fr)
         self.cb8.pack()
 
+        self.cb_items.append(self.cb1)
+        self.cb_items.append(self.cb2)
+        self.cb_items.append(self.cb3)
+        self.cb_items.append(self.cb4)
+        self.cb_items.append(self.cb5)
+        self.cb_items.append(self.cb6)
+        self.cb_items.append(self.cb7)
+        self.cb_items.append(self.cb8)
 
 
 
@@ -105,3 +121,28 @@ class GraphicView:
 
     def draw_plot(self):
         pass
+
+    def rb_tech_call(self):
+            rb_tech = self.var_tech.get()
+            rb_period = self.var_period.get()
+            
+            if rb_tech == 1 and rb_period == 1:
+                for cb in self.cb_items:
+                  cb.configure(values=self.column_2gh)
+            elif rb_tech == 1 and rb_period == 2:
+                for cb in self.cb_items:
+                  cb.configure(values=self.column_2gd)
+            elif rb_tech == 2 and rb_period == 1:
+                for cb in self.cb_items:
+                  cb.configure(values=self.column_3gh)
+            elif rb_tech == 2 and rb_period == 2:
+                for cb in self.cb_items:
+                  cb.configure(values=self.column_3gd)
+            elif rb_tech == 3 and rb_period == 1:
+                for cb in self.cb_items:
+                    cb.configure(values=self.column_4gh)
+            elif rb_tech == 3 and rb_period == 2:
+                for cb in self.cb_items:
+                    cb.configure(values=self.column_4gd)
+
+
